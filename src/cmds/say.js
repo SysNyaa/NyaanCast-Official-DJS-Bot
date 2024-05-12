6470240bf16fbe4d915702638020a4ba75533354
Nekko <58060784+SysNyaa@users.noreply.github.com>
2024-05-12 05:57:00 -0700

    Fixed Clear duplication as say
    -Fixed an accident
    I accidentally duplicated clear and replaced the say command when moving stuff around

--- src/cmds/say.js
+++ src/cmds/say.js
@@ -1,43 +1,24 @@
-// cmds/clear.js
+// cmds/say.js
 
 module.exports = {
     help: {
-        name: 'clear',
-        description: 'Clear messages from the channel.',
-        usage: '<number of messages>',
-        category: 'Moderation'
+        name: 'say',
+        description: 'Simon Says!',
+        usage: '!say <message>',
+        category: 'Utility'
     },
-    async run(c, msg, args) {
-        // Check if the user has permissions to manage messages
-        if (!msg.member.permissions.has('MANAGE_MESSAGES')) {
-            return msg.reply("You don't have permission to use this command!");
-        }
+    run(client, msg, args) {
+        // Join the args array into a single string
+        const sayMsg = args.join(' ');
 
-        // Check if a number of messages to delete is provided
-        if (!args.length || isNaN(args[0])) {
-            return msg.reply('Please provide the number of messages to clear!');
-        }
-
-        // Parse the number of messages to delete
-        let amount = parseInt(args[0]);
-
-        // Check if the amount is valid
-        if (amount <= 0 || amount > 100) {
-            return msg.reply('You can only delete between 1 and 100 messages at a time!');
-        }
-
-        // Increment the amount by 1 to account for the bot's own message
-        amount++;
-
-        // Delete the specified number of messages
-        msg.channel.bulkDelete(amount, true)
-            .then(delMsgs => {
-                message.channel.send(`Successfully deleted ${delMsgs.size - 1} messages.`)
-                    .catch(console.error);
+        // Send the say message
+        msg.channel.send(sayMsg)
+            .then(() => {
+                // Delete the command message
+                msg.delete();
             })
             .catch(error => {
-                console.error('Error deleting messages:', error);
-                msg.reply('There was an error deleting messages!');
+                console.error('Error while sending say message:', error);
             });
     }
 };
